bc65e1f38b6bab24ad6036bb8c6755392ba47b2e
Patrick Edson <pedson@users.noreply.github.com>
2019-03-14 22:23:50 -0400

    WiP - start removing hard-coded elements of slice queries.

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,23 @@
-import * as express from "express";
-import * as os from "os";
 import * as path from "path";
+import * as os from "os";
+import * as express from "express";
+import bodyParser = require("body-parser");
 
 const debug = require("debug")("mnb:static-resources:server");
 
 import {ServiceOptions} from "./options/serviceOptions";
-import {sliceMiddleware} from "./middleware/sliceMiddleware";
+import {sliceImageMiddleware, sliceMiddleware} from "./middleware/sliceMiddleware";
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
 if (process.env.NODE_ENV !== "production") {
     app.use(ServiceOptions.endpoint, express.static(path.resolve(path.normalize("assets"))));
-    app.use("/slice", sliceMiddleware);
+    app.use("/slice/", sliceMiddleware);
+    app.use("/sliceImage", sliceImageMiddleware);
 } else {
     app.use(ServiceOptions.endpoint, express.static("static"));
 }
--- src/middleware/sliceMiddleware.ts
+++ src/middleware/sliceMiddleware.ts
@@ -1,14 +1,80 @@
+import * as fs from "fs";
 import {Image as ImageJS} from "image-js";
 
 const scaling = 0xFFFF / 265;
 
-export async function sliceMiddleware(req, res) {
+enum SlicePlane {
+    Coronal,
+    Horizontal,
+    Sagittal
+}
 
-    const image = await ImageJS.load("assets/slice/sample-001/coronal/264.png");
+type Coordinates = [number, number, number]
 
-    for (let idx = 0; idx < image.data.length; idx++) {
-        image.data[idx] = Math.min((Math.max(image.data[idx] - 35, 0)), 300) * scaling;
+interface ISliceRequestBody {
+    sampleId?: string;
+    plane?: SlicePlane;
+    coordinates?: Coordinates;
+}
+
+const planeLookup = new Map<SlicePlane, string>();
+planeLookup.set(SlicePlane.Coronal, "coronal");
+planeLookup.set(SlicePlane.Horizontal, "coronal");
+planeLookup.set(SlicePlane.Sagittal, "sagittal");
+
+function locateSlice(request: ISliceRequestBody): string {
+    if (request.sampleId === undefined || request.plane === undefined || request.coordinates === undefined || request.coordinates.length !== 3 || !planeLookup.has(request.plane)) {
+        return null;
     }
 
-    res.send(image.toBase64("image/png"));
+    const index = Math.floor(request.coordinates[2] / 25);
+
+    return `assets/slice/${request.sampleId}/${planeLookup.get(request.plane)}/${index}.png`;
+}
+
+export async function sliceMiddleware(req, res) {
+    const path = locateSlice(req.body);
+
+    if (path === null || !fs.existsSync(path)) {
+        res.status(404).send("Not found");
+        return;
+    }
+
+    const image = await ImageJS.load(path);
+
+    const image2 = await ImageJS.createFrom(image, {});
+
+    for (let idx = 0; idx < image.data.length; idx++) {
+        const shifted = image.data[idx] - 35;
+        image.data[idx] = Math.min((Math.max(shifted, 0)), 300) * scaling;
+        image2.data[idx] = shifted < 0 ? 0 : 0xFFFF;
+    }
+
+    res.json({
+        texture: image.toBase64("image/png"),
+        mask: image2.toBase64("image/png")
+    });
+}
+
+export async function sliceImageMiddleware(req, res) {
+    const path = locateSlice({
+        sampleId: req.query.sampleId,
+        plane: parseInt(req.query.plane),
+        coordinates: [req.query.x, req.query.y,req.query.z]
+    });
+
+    if (path === null || !fs.existsSync(path)) {
+        res.status(404).send("Not found");
+        return;
+    }
+
+    const image = await ImageJS.load(path);
+
+    for (let idx = 0; idx < image.data.length; idx++) {
+        const shifted = image.data[idx] - 35;
+        image.data[idx] = Math.min((Math.max(shifted, 0)), 300) * scaling;
+    }
+
+    res.type("png");
+    res.end(new Buffer(image.toBuffer()), "binary");
 }
